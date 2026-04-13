16852c8ac2209c4cbac4f16bffa4ec8121aea65e
timofejkononenko931-dot <timofejkononenko931@gmail.com>
2026-04-13 13:18:55 +0500

    Update x64.cpp

--- toolChain/src/x64.cpp
+++ toolChain/src/x64.cpp
@@ -7,13 +7,17 @@ using namespace std;
 void read(string filename) {
   ifstream file(filename);
 
+  while (getline(file,line)) {
+
+  }
+  
   file.close();
 }
 
 void write(std::string outFilename) {
   ofstream out(outFilename);
 
-  if (out != out.close()) {
+  if (out.is_open()) {
 
 
     out.close();
